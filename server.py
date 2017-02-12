48b0dbe5afe4c0797dcd6cc57c44ba2ba505bd7a
Dmitry Kutsenko <d2emonium@gmail.com>
2017-02-13 02:54:28 +0300

    Rabdom encounter

--- server.py
+++ server.py
@@ -10,5 +10,26 @@ def hello():
     return render_template("index.html")
 
 
+@app.route("/encounter", methods=['GET', 'POST'])
+def encounter():
+    wilderness = [
+        {"title": "Пустынная", "chance": 5},
+        {"title": "Дикая", "chance": 8},
+        {"title": "Обитаемая", "chance": 10},
+        {"title": "Густонаселенная", "chance": 12},
+    ]
+
+    from flask import request, render_template
+    if request.method == 'POST':
+        try:
+            hours = int(request.form["hours"])
+        except ValueError:
+            hours = 0
+            pass
+    else:
+        hours = -1000
+    return render_template("encounter.html", hours=hours, wilderness=wilderness)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
