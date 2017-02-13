e90e9be0e8cdf90beba375d3652a675d70700316
Dmitry Kutsenko <d2emonium@gmail.com>
2017-02-13 18:13:04 +0300

    Encounters

--- server.py
+++ server.py
@@ -4,21 +4,29 @@ from flask import Flask
 app = Flask(__name__)
 
 
+wilderness = [
+    {"id": 0, "title": "Пустынная", "chance": 5},
+    {"id": 1, "title": "Дикая", "chance": 8},
+    {"id": 2, "title": "Обитаемая", "chance": 10},
+    {"id": 3, "title": "Густонаселенная", "chance": 12},
+]
+
+
 @app.route("/")
 def hello():
     from flask import render_template
     return render_template("index.html")
 
 
-@app.route("/encounter", methods=['GET', 'POST'])
+@app.route("/encounter")
 def encounter():
-    wilderness = [
-        {"title": "Пустынная", "chance": 5},
-        {"title": "Дикая", "chance": 8},
-        {"title": "Обитаемая", "chance": 10},
-        {"title": "Густонаселенная", "chance": 12},
-    ]
+    from flask import render_template
+    global wilderness
+    return render_template("encounter.html", wilderness=wilderness)
 
+
+@app.route("/encounter/<int:wild>", methods=['GET', 'POST'])
+def encounterPlace(wild):
     from flask import request, render_template
     if request.method == 'POST':
         try:
@@ -28,7 +36,16 @@ def encounter():
             pass
     else:
         hours = -1000
-    return render_template("encounter.html", hours=hours, wilderness=wilderness)
+
+    import dice
+    place = wilderness[wild]
+    i = 0
+    for i in range(0, hours):
+        c = dice.d(1, 100)
+        if c <= place["chance"]:
+            break
+
+    return render_template("encounter_place.html", hours=hours, eh=i, place=place)
 
 
 if __name__ == "__main__":
